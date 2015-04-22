fb88d14321a93f413975c850a17a5cc8ff0609c4
Adrien Guinet <aguinet@quarkslab.com>
2015-04-23 00:12:18 +0200

    map/fiter on variadic list of types (and std::tuple)

---
+++ cxx-idioms/map_types.cpp
@@ -0,0 +1,32 @@
+#include <tuple>
+#include <vector>
+#include <iostream>
+#include <typeinfo>
+
+template <class Mapper, class... Types>
+struct map_impl
+{
+	using type = std::tuple<typename Mapper::template bind<Types>...>;
+};
+
+template <class Mapper, class... Types>
+struct map_impl<Mapper, std::tuple<Types...>>
+{
+	using type = std::tuple<typename Mapper::template bind<Types>...>;
+};
+
+template <class Mapper, class... Types>
+using map = typename map_impl<Mapper, Types...>::type;
+
+struct map_vector
+{
+	template <class T>
+	using bind = std::vector<T>;
+};
+
+
+int main()
+{
+	using mapped = map<map_vector, std::tuple<int, std::string>>;
+	static_assert(std::is_same<mapped, std::tuple<std::vector<int>, std::vector<std::string>>>::value, "not good!");
+}
