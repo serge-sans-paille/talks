4370427a92358941f83b24110b8b5574a92e1075
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2015-04-23 11:33:42 +0200

    More idioms

---
+++ cxx-idioms/copy_elision.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+
+struct Foo {
+  const int value;
+
+  Foo(int v) : value(v) {}
+  Foo(Foo&& foo)
+  : value(foo.value)
+  {
+    puts(__PRETTY_FUNCTION__);
+  }
+  Foo(Foo const& foo)
+  : value(foo.value)
+  {
+    puts(__PRETTY_FUNCTION__);
+  }
+};
+
+Foo test() {
+  Foo f(0);
+  return f;
+}
+
+int main() {
+  Foo g(test());
+  return g.value;
+}
+
---
+++ cxx-idioms/option_type.cpp
@@ -0,0 +1,58 @@
+#include <cassert>
+#include <iostream>
+
+template<class T>
+class OptionType {
+  char  _storage[sizeof(T)];
+  T* _value;
+
+  public:
+  OptionType() : _value(nullptr) {}
+  OptionType(OptionType const&) = delete;
+  OptionType(OptionType && other)
+  {
+    if(other._value)
+      _value = new (_storage) T(std::move(*other._value));
+    else
+      _value = nullptr;
+  }
+  template<class... Args>
+  OptionType(Args&& ... args)
+    : _value{new (_storage) T(std::forward<Args>(args)...)}
+  {
+  }
+
+  operator bool() const { return _value; }
+
+  T& get() {
+    assert(_value && "not in error state");
+    return *_value;
+  }
+  T const& get() const {
+    assert(_value && "not in error state");
+    return *_value;
+  }
+};
+
+OptionType<int> maybe(int value) {
+  if(value != 0)
+    return 5 / value;
+  else
+    return {};
+};
+
+int main() {
+  if(auto v = maybe(0)) {
+    std::cout << v.get() << std::endl;
+  }
+  else {
+    std::cerr << "error handling code" << std::endl;
+  }
+  if(auto v = maybe(1)) {
+    std::cout << v.get() << std::endl;
+  }
+  else {
+    std::cerr << "error handling code" << std::endl;
+  }
+  return 0;
+}
